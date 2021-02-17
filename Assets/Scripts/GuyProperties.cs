985cb1ef0c53a1cb860d24ab2a3d94b251b2023e
knguye <kevin-nguyen@live.ca>
2021-02-17 11:47:16 -0500

    Initial Commit

---
+++ Assets/Scripts/GuyProperties.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuyProperties : MonoBehaviour
+{
+    public float walkSpeed = 2.0f; // if for CPU, set to -1
+    bool sentOut {get; set;}
+    bool isBottomGuy; // update when the list of guys is fully updated
+    [SerializeField]
+    public bool hitBoundary;
+
+    [SerializeField]
+    float power = 0; // 0 to 1, random value when sent out. higher values beat lower values
+    [SerializeField]
+    float hp = 1f;
+    public MasterController ms;
+    //public int index {get; set;} // in list
+
+    void Start()
+    {
+        isBottomGuy = false;
+        setPower();
+        ms = GameObject.Find("MasterController").GetComponent<MasterController>();
+    }
+
+    public void moveGuy(bool isPlayer){
+        if (isPlayer)   walkSpeed *= 1;
+        else            walkSpeed *= -1;
+
+        StartCoroutine(startMovement());
+    }
+
+    public IEnumerator startMovement(){
+        // Start walk animation
+        if (isBottomGuy){
+            gameObject.GetComponent<Animator>().SetBool("walk", true);
+        }
+        sentOut = true;
+
+        // Set velocity to 1
+        while (!ms.matchFinished){
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(walkSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+
+            yield return null;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+        if (col.gameObject.tag == "Boundary"){
+            Debug.Log("B");
+            if (ms.started)     ms.switchDirection(gameObject.tag);
+        }
+        if (gameObject.tag == col.gameObject.tag){
+            Debug.Log("Tx");
+            //StartCoroutine(switchDirection(col.gameObject));
+        }
+        else if (col.gameObject.tag == "LB"){
+            Debug.Log("LB");
+            StartCoroutine(switchDirection(2));
+            //ms.switchDirection(gameObject.tag);
+        }
+        else if (col.gameObject.tag == "RB"){
+            Debug.Log("RB");
+            StartCoroutine(switchDirection(-2));
+        }
+        else if (col.gameObject.tag != gameObject.tag && col.gameObject.tag != "Boundary" && col.gameObject.tag != "Untagged"){
+            Debug.Log("A");// Remove this object if power is less than the enemy
+            //if (power < col.gameObject.GetComponent<GuyProperties>().power){
+            col.gameObject.GetComponent<AudioSource>().Play(0);
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(walkSpeed * 40f, 15f), ForceMode2D.Impulse);
+            col.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(walkSpeed * 40f, 15f), ForceMode2D.Impulse);
+            
+            hp -= col.gameObject.GetComponent<GuyProperties>().power;
+                
+            if (hp <= 0) {
+                ms.GetComponent<MasterController>().destroyUnit(gameObject);
+            }
+        }
+    }
+
+    public void OnCollisionStay2D(Collision2D col){
+        if (gameObject.tag == col.gameObject.tag){
+            Debug.Log("Tx");
+            //StartCoroutine(switchDirection(col.gameObject));
+        }
+    }
+
+
+    IEnumerator switchDirection(int direction){
+        walkSpeed = direction;
+        //gameObject.GetComponent<Rigidbody2D>().AddForce(transform. * 5f, ForceMode2D.Impulse);
+        hitBoundary = true;
+        yield return new WaitForSeconds(2f);
+    }
+
+    IEnumerator switchDirection(GameObject ally){
+        if (hitBoundary){
+            ally.GetComponent<GuyProperties>().walkSpeed = walkSpeed;
+            ally.GetComponent<GuyProperties>().hitBoundary = true;
+        }
+        yield return new WaitForSeconds(5f);
+    }
+
+    void setPower(){
+        power = Random.Range(0, 1.0f);
+    }
+
+}
---
+++ Assets/Scripts/MasterController.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MasterController : MonoBehaviour
+{
+    public List<GameObject> playerUnits; // Your guys
+    public GameObject singleUnit; // The skin of your units
+
+    public List<GameObject> cpuUnits; // Computer's units
+    public GameObject cpuSingleUnit;
+
+    public Transform spawnPosition;
+
+    public Text unitCounter;
+    int totalUnits = 10; // Total units that the player "owns"
+    int placedUnits = 0; // Total
+
+    public Transform cpuSpawnPosition;
+
+    public Text cpuCounter;
+    int cpuPlacedUnits = 0;
+    int cpuTotalUnits = 10;
+
+    public bool started {get; set;}
+    public bool matchFinished {get; set;}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerUnits = new List<GameObject>();
+        cpuUnits = new List<GameObject>();
+        started = false;
+        matchFinished = false;
+        updateUnitCount();
+        setupCPU();
+        updateCPUCount();
+    }
+
+    public void sendOut(){
+        if (!started){
+            foreach ( GameObject unit in playerUnits){
+                unit.GetComponent<GuyProperties>().moveGuy(true);
+            }
+
+            // Release CPU units as well
+            foreach ( GameObject unit in cpuUnits) {
+                unit.GetComponent<GuyProperties>().moveGuy(false);
+            }
+        }
+        started = true;
+    }
+
+    public void increaseUnits(bool isPlayer){
+
+        // For Players
+        // Instantiate unit if we don't exceed the totalUnits we have.
+        if (isPlayer){
+            if (placedUnits < totalUnits){
+                GameObject createdUnit = Instantiate(singleUnit, spawnPosition, true);
+                // Add the unit to the list
+                playerUnits.Add(createdUnit);
+                placedUnits++;
+            }
+        }
+        else {
+            GameObject createdUnit = Instantiate(cpuSingleUnit, cpuSpawnPosition, true);
+            cpuUnits.Add(createdUnit);
+        }
+        // For CPUs
+
+    }
+
+    public void decreaseUnits(){
+        if (placedUnits > 0){
+            playerUnits[playerUnits.Count-1].SetActive(false);
+            playerUnits.RemoveAt(playerUnits.Count-1);
+            placedUnits--;
+        }
+    }
+
+    // Called when power of a colliding unit is less than the enemy
+    public void destroyUnit(GameObject unit){
+        Debug.Log(unit.tag);
+        if (unit.tag == "Player"){
+            unit.SetActive(false);
+            playerUnits.Remove(unit);
+            placedUnits--;
+            totalUnits--;
+            updateUnitCount();
+            if (placedUnits <= 0){
+                fightOver(true);// You lose
+            }
+        }
+        else {
+            unit.SetActive(false);
+            cpuUnits.Remove(unit);
+            cpuPlacedUnits--;
+            cpuTotalUnits--;
+            updateCPUCount();
+            if (cpuPlacedUnits <= 0){
+                fightOver(false);// They lose
+            }
+        }
+
+    }
+
+    public void switchDirection(string unitTag){
+        Debug.Log(unitTag);
+        if (unitTag == "Enemy"){
+            foreach (GameObject unit in cpuUnits){
+                unit.GetComponent<GuyProperties>().walkSpeed *= -1;
+                StartCoroutine(unit.GetComponent<GuyProperties>().startMovement());
+            }
+        }
+        else if (unitTag == "Player"){
+            foreach (GameObject unit in playerUnits){
+                unit.GetComponent<GuyProperties>().walkSpeed *= -1;
+                StartCoroutine(unit.GetComponent<GuyProperties>().startMovement());
+            }
+        }
+    }
+
+    public void setupCPU(){
+        cpuPlacedUnits = Random.Range(1, cpuTotalUnits);
+        for (int i = 0; i < cpuPlacedUnits; i++){
+            increaseUnits(false);
+        }
+    }
+
+    public void fightOver(bool playerWon){
+        matchFinished = true;
+        if (playerWon){
+            // Display player wins
+            // Give territory to player
+        }
+        else {
+            // Display CPU wins
+            // Give territory to CPU
+        }
+    }
+
+    public void updateUnitCount(){
+        unitCounter.text = placedUnits.ToString() + " / " + totalUnits.ToString();
+    }
+
+    public void updateCPUCount(){
+        cpuCounter.text = cpuPlacedUnits.ToString() + " / " + cpuTotalUnits.ToString();
+    }
+}
